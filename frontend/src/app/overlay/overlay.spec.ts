09902ea8c3feb84077d3e4a8c7858e76698a02cc
= <hannes.vinzens@fau.de>
2026-01-06 22:08:24 +0100

    port to daisy ui

--- frontend/src/app/overlay/overlay.spec.ts
+++ frontend/src/app/overlay/overlay.spec.ts
@@ -1,23 +1,22 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { type ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { Overlay } from './overlay';
+import { Overlay } from "./overlay";
 
-describe('Overlay', () => {
-  let component: Overlay;
-  let fixture: ComponentFixture<Overlay>;
+describe("Overlay", () => {
+	let component: Overlay;
+	let fixture: ComponentFixture<Overlay>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [Overlay]
-    })
-    .compileComponents();
+	beforeEach(async () => {
+		await TestBed.configureTestingModule({
+			imports: [Overlay],
+		}).compileComponents();
 
-    fixture = TestBed.createComponent(Overlay);
-    component = fixture.componentInstance;
-    await fixture.whenStable();
-  });
+		fixture = TestBed.createComponent(Overlay);
+		component = fixture.componentInstance;
+		await fixture.whenStable();
+	});
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+	it("should create", () => {
+		expect(component).toBeTruthy();
+	});
 });
